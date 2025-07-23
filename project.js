e0d0912ed9301a90849701f0acdb8967028fd863
DineshPulapalli <dineshpulapalli@gmail.com>
2025-07-23 15:17:06 +0530

    addded black text

--- project.js
+++ project.js
@@ -3,6 +3,9 @@ function switchOn() {
     document.getElementById("catImage").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-img.png"
     document.getElementById("switchStatus").textContent="Switched On"
      document.getElementById("onSwitch").style.backgroundColor="#cbd2d9"
+     document.getElementById("onSwitch").style.color="#000000ca"
+     document.getElementById("offSwitch").style.color="#ffffff"
+
      document.getElementById("offSwitch").style.backgroundColor="#e12d39"
     }
     function switchOff() {
@@ -11,5 +14,7 @@ function switchOn() {
         document.getElementById("switchStatus").textContent="Switched Off"
         document.getElementById("offSwitch").style.backgroundColor="#cbd2d9"
         document.getElementById("onSwitch").style.backgroundColor="#22c55e"
-   
+    document.getElementById("offSwitch").style.color="#000000ca"
+     document.getElementById("onSwitch").style.color="#ffffff"
+
 }
