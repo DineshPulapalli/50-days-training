0a71cabc4f94acfd15dcc412a60d870ae04f48e2
DineshPulapalli <dineshpulapalli@gmail.com>
2025-07-23 13:59:33 +0530

    .

---
+++ 22-07-25.js
@@ -0,0 +1,4 @@
+function changeData() {
+    
+ document.getElementById("headElement").textContent="4.0";
+}
---
+++ project.js
@@ -0,0 +1,6 @@
+function switchOn() {
+    document.getElementById("bulbImage").scr="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png"
+}
+function switchOff() {
+    document.getElementById("bulbImage").scr="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png"
+}
